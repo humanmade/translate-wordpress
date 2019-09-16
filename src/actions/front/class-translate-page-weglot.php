1312cc47b9ca3008315f87f3f603388f440f2993
Julien MA JACOB <juulien.jacob@gmail.com>
2019-09-16 16:43:04 +0200

    Fix custom URL with parameters

--- src/actions/front/class-translate-page-weglot.php
+++ src/actions/front/class-translate-page-weglot.php
@@ -220,6 +220,10 @@ class Translate_Page_Weglot implements Hooks_Interface_Weglot {
 		) ), 'strlen' ));
 
 		$index_entries = count( $request_without_language ) - 1;
+		if ( 0 === strpos($request_without_language[$index_entries], '?') ) {
+			$index_entries = count( $request_without_language ) - 2;
+		}
+
 		if ( isset( $request_without_language[ $index_entries ] ) ) {
 			$slug_in_work  = $request_without_language[ $index_entries ];
 		}
